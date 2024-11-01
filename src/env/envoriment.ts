6422f9aef1b9a2c2a1176a68ae285976339d182a
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-11-01 10:59:14 -0300

    feat/discord-config, configurações adicionadas para o funcionamento com a integração com o discord e alguma funcionalidades da api.

---
+++ src/env/envoriment.ts
@@ -0,0 +1,5 @@
+
+
+export const discordFailureUrl = process.env.DISCORD_FAILURE_WEBHOOK || '';
+export const discordSuccessUrl = process.env.DISCORD_SUCCESS_WEBHOOK || '';
+export const nestjsUrl = process.env.APP_URL || '';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sendDiscordMessage } from './utils/discord.util';
+import { Logger } from '@nestjs/common';
+import { discordFailureUrl, nestjsUrl } from './env/envoriment';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    credentials: true,
+  });
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Escola API')
+    .setDescription('API para uma faculdade de desenvolvimento web')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(nestjsUrl, () => {
+    console.log(`Aplicação iniciada em http://localhost:${nestjsUrl}/api`);
+  });
 }
-bootstrap();
+bootstrap().catch(async (e) => {
+  new Logger('main.ts').error(e);
+
+  const url = discordFailureUrl
+  const msg = typeof e?.message === 'string' ? e.message : '';
+
+  await sendDiscordMessage(
+    url,
+    `Aplicação Falhou ao Iniciar: ${msg}\nSinto Muito ( u.u)`,
+    isProd,
+    false,
+  );
+  if (isProd) await new Promise((r) => setTimeout(r, 3600_000));
+  else throw e;
+});
